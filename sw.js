737d3ef985d26ba00587a9c715317f027b7aadd0
Arihant Jain <arihant427@gmail.com>
2021-05-11 01:17:41 +0530

    Add chrome notification

---
+++ sw.js
@@ -0,0 +1,24 @@
+// client/sw.js
+    
+self.addEventListener('push', event => {
+    const data = event.data.json();
+    self.registration.showNotification(data.title, {
+      body: data.response,
+      icon:"assets/img/logo6.png",
+    });
+  });
+
+  //notification url redirect event click
+self.addEventListener('notificationclick', function (event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({
+            type: "window"
+        })
+        .then(function (clientList) {
+            if (clients.openWindow) {
+                return clients.openWindow("https://selfregistration.cowin.gov.in/");
+            }
+        })
+    );
+});
