78a40ace3a0445a2c6421db829f6001d56879380
Arihant Jain <arihant427@gmail.com>
2021-05-11 11:33:32 +0530

    Add action button

--- sw.js
+++ sw.js
@@ -5,19 +5,27 @@ self.addEventListener('push', event => {
     self.registration.showNotification(data.title, {
       body: data.response,
       icon:"assets/img/logo6.png",
+      actions: [{action: data.action, title: data.actionName}],
       vibrate: [2000, 500, 1000],
     });
   });
 
   //notification url redirect event click
 self.addEventListener('notificationclick', function (event) {
-    event.notification.close();
+    //event.notification.close();
     event.waitUntil(
         clients.matchAll({
             type: "window"
         })
         .then(function (clientList) {
-            if (clients.openWindow) {
+            if (event.action === 'visit'&& clients.openWindow)  {
+                return clients.openWindow("https://selfregistration.cowin.gov.in/");
+            }else 
+            if (event.action === 'book'&& clients.openWindow)  {
+
+                return clients.openWindow("https://selfregistration.cowin.gov.in/");
+              } 
+            else if (clients.openWindow) {
                 return clients.openWindow("https://selfregistration.cowin.gov.in/");
             }
         })
